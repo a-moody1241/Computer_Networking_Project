88abaacd89be1a7467ac5f8ee205c64d95bf8e8d
catherinehealy04 <catherinehealy04@gmail.com>
2021-10-19 15:04:30 -0400

    CommonPeerProperties_addition

---
+++ src/CommonPeerProperties.java
@@ -0,0 +1,70 @@
+import java.io.*;
+
+public class CommonPeerProperties {
+
+    public static final String CONFIG_FILE = "Common.cfg";
+    private static int numberOfPreferredNeighbors;
+    private static int unchokingInterval;
+    private static int optimisticUnchokingInterval;
+    private static String fileName;
+    private static int fileSize;
+    private static int pieceSize;
+
+    public void read() {
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(CONFIG_FILE));
+            String line;
+            while ((line = br.readLine()) != null) {
+                int deliminatorIndex = line.indexOf(' ');
+                String property = line.substring(0,deliminatorIndex);
+                String value = line.substring(deliminatorIndex+1);
+
+                switch (property){
+                    case "NumberOfPreferredNeighbors":
+                        numberOfPreferredNeighbors = Integer.parseInt(value) + 1;
+                        break;
+                    case "UnchokingInterval":
+                        unchokingInterval = Integer.parseInt(value);
+                        break;
+                    case "OptimisticUnchokingInterval":
+                        optimisticUnchokingInterval = Integer.parseInt(value);
+                        break;
+                    case "FileName":
+                        fileName = value;
+                        break;
+                    case "FileSize":
+                        fileSize = Integer.parseInt(value);
+                        break;
+                    case "PieceSize":
+                        pieceSize = Integer.parseInt(value);
+                        break;
+                }
+            }
+            br.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static String getConfigFile() { return CONFIG_FILE; }
+
+    public static int getNumberOfPreferredNeighbors() { return numberOfPreferredNeighbors; }
+    public static void setNumberOfPreferredNeighbors(int numberOfPreferredNeighbors) { CommonPeerProperties.numberOfPreferredNeighbors = numberOfPreferredNeighbors; }
+
+    public static int getUnchokingInterval() { return unchokingInterval; }
+    public static void setUnchokingInterval(int unchokingInterval) { CommonPeerProperties.unchokingInterval = unchokingInterval; }
+
+    public static int getOptimisticUnchokingInterval() { return optimisticUnchokingInterval; }
+    public static void setOptimisticUnchokingInterval(int optimisticUnchokingInterval) { CommonPeerProperties.optimisticUnchokingInterval = optimisticUnchokingInterval; }
+
+    public static String getFileName() { return fileName; }
+    public static void setFileName(String fileName) { CommonPeerProperties.fileName = fileName; }
+
+    public static int getFileSize() { return fileSize; }
+    public static void setFileSize(int fileSize) { CommonPeerProperties.fileSize = fileSize; }
+
+    public static int getPieceSize() { return pieceSize; }
+    public static void setPieceSize(int pieceSize) { CommonPeerProperties.pieceSize = pieceSize; }
+
+
+}
--- src/Server.java
+++ src/Server.java
@@ -20,8 +20,7 @@ public class Server {
             			}
         	} finally {
             		listener.close();
-        	} 
- 
+        	}
     	}
 
 	/**
@@ -29,12 +28,12 @@ public class Server {
      	* loop and are responsible for dealing with a single client's requests.
      	*/
     	private static class Handler extends Thread {
-        	private String message;    //message received from the client
-		private String MESSAGE;    //uppercase message send to the client
-		private Socket connection;
+    		private String message;    //message received from the client
+			private String MESSAGE;    //uppercase message send to the client
+			private Socket connection;
         	private ObjectInputStream in;	//stream read from the socket
         	private ObjectOutputStream out;    //stream write to the socket
-		private int no;		//The index number of the client
+			private int no;		//The index number of the client
 
         	public Handler(Socket connection, int no) {
             		this.connection = connection;
@@ -42,6 +41,9 @@ public class Server {
         	}
 
         public void run() {
+
+
+
  		try{
 			//initialize Input and Output streams
 			out = new ObjectOutputStream(connection.getOutputStream());
