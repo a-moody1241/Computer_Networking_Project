ccf4196534452b4ae26571e1058bcc074dd912a8
Anna Moody <a.moody1241@gmail.com>
2021-10-21 14:09:58 -0400

    peer process

--- src/peerProcess.java
+++ src/peerProcess.java
@@ -1,5 +1,9 @@
+
 public class peerProcess {
     public static void main(String[] args){
         int peerID = Integer.parseInt(args[0]);
+        commonpeerproperties cpp = new CommonPeerPropties;
+        cpp.read();
+        cpp.get(filename);
     }
 }
