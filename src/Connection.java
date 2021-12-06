85bc1c98719a3aa4aa3be5479c9be7ed35756bb4
Anna Moody <a.moody1241@gmail.com>
2021-12-06 15:56:41 -0500

    delete

--- src/Connection.java
+++ src/Connection.java
@@ -119,7 +119,6 @@ public class Connection {
                                         sendRequest();
                                     }
                                     break;
-
                             }
                         }
                     } catch (SocketException e){
